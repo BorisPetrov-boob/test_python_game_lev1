7ae488e1060f0b6c562fa44c59b39610f853d5da
Admin <yusk8038@gmail.com>
2025-08-22 21:32:23 +0300

    Пример без использования декоратора

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("first commit")
+
+
+def say_hello(name):
+    print (f"Hello,{name}")
+
+# вместо декоратора создаем функцию обертку
+def repeat_func(func, num_times, *args, ** kwargs):
+    for _ in range(num_times):
+        func(*args, **kwargs)
+
+
+# вызываем функцию через обертку
+
+repeat_func(say_hello,3, "Alice")
